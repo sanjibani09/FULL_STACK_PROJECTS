e9caa485ad8bfa9df84f2bd87eb1a418c2475555
Sanjibani <sanjibanipaul4@gmail.com>
2026-07-17 10:36:39 +0530

    EXP 1

--- multiple-platforms/src/components/charactercounter.jsx
+++
@@ -1,7 +0,0 @@
-function CharacterCounter({ count, limit }) {
-  return (
-    <span className="counter" aria-label={`${count} of ${limit} characters used`}>{count} / {limit}</span>
-  );
-}
-
-export default CharacterCounter;
--- multiple-platforms/src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      React Lab Assignment
-    </footer>
-  );
-}
-
-export default Footer;
